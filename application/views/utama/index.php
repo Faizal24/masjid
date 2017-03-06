6f1930102aea19a88631bd062d2335359d2255cc
syafiqsinwan <syafiq.freelance@gmail.com>
2017-03-07 01:07:18 +0800

    add blog
    berita macam ni

--- application/views/utama/index.php
+++ application/views/utama/index.php
@@ -12,3 +12,55 @@
             </div>
         </div>
     </header>
+   <section class="bg-primary" id="whenwhere">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-3 well">
+                    <h1 style="color: black">Waktu Solat</h1>
+                </div>
+                <div class="col-md-9">
+                    <div class="row" style="background-color: blue">
+                        <h1>Demo post with formatted elements and comments.</h1><br>
+                    </div>
+                    <div class="row" style="background-color: blue;">
+                        <div class="col-md-3 well">
+                            <img src="img/marissa.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-9">
+                            <p class="gallery">Aquarium. Photo by Francisco.Sorbet is the template number 4544 in BTemplates, where a total of 50 003 086 templates has been downloaded since 2008. This template was created by NBThemes and is the...</p><br>
+                            <a href="#">READ MORE</a><br><br>
+                            <p>Siapa || Tarikh || Masa</p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row" style="background-color: red">
+                        <h1>Demo post with formatted elements and comments.</h1><br>
+                    </div>
+                    <div class="row" style="background-color: red">
+                        <div class="col-md-3 col-sm-push-9 well">
+                            <img src="img/marissa.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-9 col-sm-pull-3">
+                            <p class="gallery">Aquarium. Photo by Francisco.Sorbet is the template number 4544 in BTemplates, where a total of 50 003 086 templates has been downloaded since 2008. This template was created by NBThemes and is the...</p><br>
+                            <a href="#">READ MORE</a><br><br>
+                            <p>Siapa || Tarikh || Masa</p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row" style="background-color: green">
+                        <h1>Demo post with formatted elements and comments.</h1><br>
+                    </div>
+                    <div class="row" style="background-color: green">
+                        <div class="col-md-3 well">
+                            <img src="img/marissa.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-9">
+                            <p class="gallery">Aquarium. Photo by Francisco.Sorbet is the template number 4544 in BTemplates, where a total of 50 003 086 templates has been downloaded since 2008. This template was created by NBThemes and is the...</p><br>
+                            <a href="#">READ MORE</a><br><br>
+                            <p>Siapa || Tarikh || Masa</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
