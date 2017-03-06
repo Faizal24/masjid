f24e96d8b615fcde227213a0d106809b043bbb62
syafiqsinwan <syafiq.freelance@gmail.com>
2017-03-06 20:51:11 +0800

    syafiq -  6/3 - 8:50
    Update header, footer & utama. Adding css and js file.

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,8 +1,87 @@
-<a href="utama">Utama</a>
-<a href="profil">Profil</a>
-<a href="organisasi">Organisasi</a>
-<a href="kemudahan">Kemudahan</a>
-<a href="jadual">Jadual</a>
-<a href="maahadtahfiz">Maahad Tahfiz</a>
-<a href="hubungikami">Hubungi Kami</a>
-<br>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>Masjid Kg. Kuala Pangsun</title>
+	<!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <h3>MASJID PANGSUN</h3>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="utama">UTAMA</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="profil" class="dropdown-toggle" data-toggle="dropdown">PROFIL <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">PERUTUSAN NAZIR MASJID</a></li>
+                            <li><a href="#">SEJARAH MASJID</a></li>
+                            <li><a href="#">VISI & MISI</a></li>
+                            <li><a href="#">FUNGSI & OBJEKTIF</a></li>
+                            <li><a href="#">PIAGAM PELANGGAN</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="organisasi" class="dropdown-toggle" data-toggle="dropdown">ORGANISASI <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">CARTA ORGANISASI</a></li>
+                            <li><a href="#">DIREKTORI STAFF</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="kemudahan" class="dropdown-toggle" data-toggle="dropdown">KEMUDAHAN <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">DEWAN SOLAT</a></li>
+                            <li><a href="#">DEWAN SERBAGUNA</a></li>
+                            <li><a href="#">PENGURUSAN JENAZAH</a></li>
+                            <li><a href="#">TEMPAT MELETAK KENDERAAN</a></li>
+                            <li><a href="#">PERKHIDMATAN JURUNIKAH</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="jadual">JADUAL WAKTU KULIAH</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="maahadtahfiz" class="dropdown-toggle" data-toggle="dropdown">MAAHAD TAHFIZ <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">CARTA ORGANISASI</a></li>
+                            <li><a href="#">PROFIL PELAJAR</a></li>
+                            <li><a href="#">PRESTASI PELAJAR</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="hubungikami">HUBUNGI KAMI</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
--- application/views/utama/index.php
+++ application/views/utama/index.php
@@ -1,1 +1,14 @@
-Utama...kan keselamatan anda
+	<header class="img-responsive">
+        <div class="header-content">
+            <div class="header-content-inner" id="homeHeading">
+                <div>
+                    <h1>بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم</h1><br>
+                    <h2>Selamat datang ke</h2><br>
+                    <h1>Masjid Kg. Kuala Pangsun</h1>
+                    <h4>Batu 22 1/2, Kg Kuala Pangsun, 43100 Hulu Langat, Selangor Darul Ehsan.</h4>
+
+                    <!-- <div><object width="560" height="420"><param name="movie" value="http://pf.kizoa.com/sflite.swf?did=4523837&amp;k=4939993" /><param name="wmode" value="transparent" /><param name="allowFullScreen" value="true" /><embed src="http://pf.kizoa.com/sflite.swf?did=4523837&k=4939993" type="application/x-shockwave-flash" wmode="transparent" width="560" height="420" allowfullscreen="true"></embed></object><br /><a href="http://www.kizoa.com/slideshow/d4523837k4939993o2/masjid-pangsun"><b>Masjid Pangsun</b></a> - <i><a href="http://www.kizoa.com">slideshows</a></i></div> -->
+                </div>
+            </div>
+        </div>
+    </header>
