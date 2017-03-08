190a96c1189c5c7a8a281c527d5d76736cb6b19f
syafiqsinwan <syafiq.freelance@gmail.com>
2017-03-08 17:09:13 +0800

    8/3 - Wed - 5:08pm
    Change header, utama dan hubungi kami

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -85,3 +85,31 @@
             </div>
         </div>
     </nav>
+
+    <header class="img-responsive">
+        <div class="header-content">
+            <div class="header-content-inner" id="homeHeading">
+                <div>
+                    <h1>بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم</h1><br>
+                    <h2>Selamat datang ke</h2><br>
+                    <h1>Masjid Kg. Kuala Pangsun</h1>
+                    <h4>Batu 22 1/2, Kg Kuala Pangsun, 43100 Hulu Langat, Selangor Darul Ehsan.</h4>
+                </div>
+            </div>
+        </div>
+    </header>
+   <section class="bg-primary" id="whenwhere" style="background-color: green;">
+        <div class="container">
+            <div class="row col-md-10 col-md-offset-1" style="margin-top: -50px; text-align: center;">
+                <center>SAHAM AKHIRAT ANDA - RAYUAN INFAQ UNTUK TABUNG MASJID PANGSUN MELALUI PAY PAL DENGAN KAD VISA ATAU MASTER CARD TERUS KE AKAUN "MASJID KG KUALA PANSOON" DI RHB ISLAMIC BANK BERHAD</center><br>
+                <center>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHTwYJKoZIhvcNAQcEoIIHQDCCBzwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCbukz953zXuoGLCvMRQlnPZbEVV1kQkfgAdeUUuQIVILWZVk1OI3+rnAFvPiVa+UivKoWsjPUW2p3casf+GgsLwzKnNNEVv3lgnsNKxXcTBkAfLow31pQYlHuZAXWURBQm8frbxN/3khgF+oxV6U3/w9piAFXcIliBBQXz5l8ERzELMAkGBSsOAwIaBQAwgcwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIn+wuekeVe7WAgagf/9x/QtGJORNQ6dGglaSwc8QjXhKRkO4rXqEQl1d22LE0Fekt6y96Z+en2kGOrjiAy+UdFrZ5gPuhkNs7aSLHZb1W05AzXo1aWwqkxC6CPuel3MHrlNKQm46WCoUVvd1JZv9HY2KGGpU1Ovgz1mfqyG1uUi9aDY6rRwN4DblJQUVaU/Gv0WB/sqAzamIQieHqrF6qjlgDUK3EWrOjXx18jNVtQ6bS+bWgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMjEwMjUwMTI0NDRaMCMGCSqGSIb3DQEJBDEWBBThLzXo/O6uzO8z/tZ/+5ZsNWO7wDANBgkqhkiG9w0BAQEFAASBgDXtS03FoAwoBPzRhxHl6571+3ChK1W2kFWFrCDTPJJqZCLhI3QOFfGM3J98tMSzUfZ3+1ZmDuk6lTGrekFd8xEPYV4EK4tqe2dlVhjCVyrb4gkaB8ndIew/yxDAebBLLFoi2wEGUOH20Hf73jORFx7Hh3ANUE43hf/68kGS2rGU-----END PKCS7-----
+                        " />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                    </form>
+                </center><br>
+                <center><font size="2"><p>AKAUN MASJID : RHB ISLAMIC BANK.  No. A/k : 26209000009609  .<br>SUMBANGAN CEK/WANG POS ATAS NAMA : " MASJID KG KUALA PANSOON "</p></font></center>
+            </div>
--- application/views/utama/index.php
+++ application/views/utama/index.php
@@ -1,64 +1,35 @@
-	<header class="img-responsive">
-        <div class="header-content">
-            <div class="header-content-inner" id="homeHeading">
-                <div>
-                    <h1>بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم</h1><br>
-                    <h2>Selamat datang ke</h2><br>
-                    <h1>Masjid Kg. Kuala Pangsun</h1>
-                    <h4>Batu 22 1/2, Kg Kuala Pangsun, 43100 Hulu Langat, Selangor Darul Ehsan.</h4>
-                </div>
-            </div>
-        </div>
-    </header>
-   <section class="bg-primary" id="whenwhere" style="background-color: green">
-        <div class="container">
-            <div class="row col-md-10 col-md-offset-1" style="margin-top: -50px; text-align: center;">
-                <center>SAHAM AKHIRAT ANDA - RAYUAN INFAQ UNTUK TABUNG MASJID PANGSUN MELALUI PAY PAL DENGAN KAD VISA ATAU MASTER CARD TERUS KE AKAUN "MASJID KG KUALA PANSOON" DI RHB ISLAMIC BANK BERHAD</center><br>
-                <center>
-                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                        <input type="hidden" name="cmd" value="_s-xclick" />
-                        <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHTwYJKoZIhvcNAQcEoIIHQDCCBzwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCbukz953zXuoGLCvMRQlnPZbEVV1kQkfgAdeUUuQIVILWZVk1OI3+rnAFvPiVa+UivKoWsjPUW2p3casf+GgsLwzKnNNEVv3lgnsNKxXcTBkAfLow31pQYlHuZAXWURBQm8frbxN/3khgF+oxV6U3/w9piAFXcIliBBQXz5l8ERzELMAkGBSsOAwIaBQAwgcwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIn+wuekeVe7WAgagf/9x/QtGJORNQ6dGglaSwc8QjXhKRkO4rXqEQl1d22LE0Fekt6y96Z+en2kGOrjiAy+UdFrZ5gPuhkNs7aSLHZb1W05AzXo1aWwqkxC6CPuel3MHrlNKQm46WCoUVvd1JZv9HY2KGGpU1Ovgz1mfqyG1uUi9aDY6rRwN4DblJQUVaU/Gv0WB/sqAzamIQieHqrF6qjlgDUK3EWrOjXx18jNVtQ6bS+bWgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMjEwMjUwMTI0NDRaMCMGCSqGSIb3DQEJBDEWBBThLzXo/O6uzO8z/tZ/+5ZsNWO7wDANBgkqhkiG9w0BAQEFAASBgDXtS03FoAwoBPzRhxHl6571+3ChK1W2kFWFrCDTPJJqZCLhI3QOFfGM3J98tMSzUfZ3+1ZmDuk6lTGrekFd8xEPYV4EK4tqe2dlVhjCVyrb4gkaB8ndIew/yxDAebBLLFoi2wEGUOH20Hf73jORFx7Hh3ANUE43hf/68kGS2rGU-----END PKCS7-----
-                        " />
-                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
-                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-                    </form>
-                </center><br>
-                <center><font size="2"><p>AKAUN MASJID : RHB ISLAMIC BANK.  No. A/k : 26209000009609  .<br>SUMBANGAN CEK/WANG POS ATAS NAMA : " MASJID KG KUALA PANSOON "</p></font></center>
-            </div>
             <br>
             <div class="row text-center">
-                <div class="col-md-3">
-                    <div class="row">
-                        <div style="margin-left: 50px;">
-                            <iframe marginheight="0" marginwidth="0" scrolling="no"  src=http://www2.e-solat.gov.my/solatF.php?kod=SGR04&lang=BM&url=http:/www.islam.gov.my width=260 height=330 frameborder=0></iframe>
+                <div class="col-md-2">
+                        <div>
+                            <!-- <iframe marginheight="0" marginwidth="0" scrolling="no"  src=http://www2.e-solat.gov.my/solatF.php?kod=SGR01&lang=BM&url=http:/www.islam.gov.my width=220 height=310 frameborder=0></iframe> -->
                             <!-- <script language="JavaScript" src="http://www.waktusolat.net/widget/addon.php" type="text/javascript"></script> -->
                             <!-- <script language="javascript" src="http://widget.solatfardhu.com/waktusolat/?location_code=135"></script> -->
                             <!-- <iframe marginheight="0" marginwidth="0" scrolling="no"  src=http://www2.e-solat.gov.my/solat.php?kod=SGR01&lang=BM&url=http:/www.islam.gov.my width=280 height=330 frameborder=0></iframe> -->
                             <!-- <script language="JavaScript" src="http://www.waktusolat.net/widget/addon.php"></script> -->
-                            <!-- <iframe marginheight="0" marginwidth="0" scrolling="no"  src=http://www2.e-solat.gov.my/solatD.php?kod=SGR01&lang=BM&url=http:/www.islam.gov.my width=260 height=330 frameborder=0></iframe> -->
+                            <iframe marginheight="0" marginwidth="0" scrolling="no"  src=http://www2.e-solat.gov.my/solatD.php?kod=SGR01&lang=BM&url=http:/www.islam.gov.my width=230 height=330 frameborder=0 style="margin-left: -50px;"></iframe>
                         </div>
-                        <div>
-                            <div><object width="280" height="260"><param name="movie" value="http://pf.kizoa.com/sflite.swf?did=4523837&amp;k=4939993" /><param name="wmode" value="transparent" /><param name="allowFullScreen" value="true" /><embed src="http://pf.kizoa.com/sflite.swf?did=4523837&k=4939993" type="application/x-shockwave-flash" wmode="transparent" width="280" height="260" allowfullscreen="true"></embed></object><br /><a href="http://www.kizoa.com/slideshow/d4523837k4939993o2/masjid-pangsun"><b>Masjid Pangsun</b></a> - <i><a href="http://www.kizoa.com">slideshows</a></i></div>
+                    <div class="row">
+                        <div class="well" style="margin-left: -20px; height: 60px; width: 215px; background-color: gold; border-style: none;">
+                            <p style="color: red">Pautan Laman Web</p>
                         </div>
-                        <br>
-                        <div class="well">
-                            <h3 style="color: black">Pautan Laman Web</h3><br>
+                        <div class="well" style="margin-left: -20px; margin-top: -23px;">
                             <a href="http://www.zakatselangor.com.my/" target="_blank"><img src="img/zakat.png" class="img-responsive"><br><p style="color: blue">Lembaga Zakat Selangor</p></a><hr>
                             <a href="http://www.tabunghaji.gov.my/" target="_blank"><img src="img/tabunghaji.png" class="img-responsive"><br><p style="color: blue">Lembaga Tabung Haji</p></a><hr>
-                            <a href="http://www.jais.gov.my/" target="_blank"><img src="img/jais.png" class="img-responsive"><<br><p style="color: blue">Jabatan Agama Islam Selangor</p></a><hr>
+                            <a href="http://www.jais.gov.my/" target="_blank"><img src="img/jais.png" class="img-responsive"><br><p style="color: blue">Jabatan Agama Islam Selangor</p></a><hr>
                             <a href="http://www.selangor.gov.my/" target="_blank"><img src="img/selangor.png" class="img-responsive" style="margin: auto;"><br><p style="color: blue">Kerajaan Negeri Selangor</p></a><hr>
                             <a href="http://www.mais.gov.my/" target="_blank"><img src="img/mais.png" class="img-responsive" style="margin: auto;"><br><p style="color: blue">Majlis Agama Islam Selangor</p></a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-9">
+                <div class="col-md-7">
                     <div class="well" style="background-color: blue">
                         <div class="row">
                             <h1 class="form-section">Title</h1><hr><br>
                         </div>
                         <div class="row">
                             <div class="col-md-4" style="margin: auto;">
-                                <img src="img/anuar.png" class="img-responsive" style="margin: auto;">
+                                <img src="img/bg_classmate.jpg" class="img-responsive" style="margin: auto;">
                             </div>
                             <div class="col-md-8">
                                 <p>Aquarium. Photo by Francisco.Sorbet is the template number 4544 in BTemplates, where a total of 50 003 086 templates has been downloaded since 2008. This template was created by NBThemes and is the...</p><br>
@@ -74,7 +45,7 @@
                         </div>
                         <div class="row">
                             <div class="col-md-4 col-md-push-8" style="margin: auto;">
-                                <img src="img/anuar.png" class="img-responsive" style="margin: auto;">
+                                <img src="img/bg_classmate.jpg" class="img-responsive" style="margin: auto;">
                             </div>
                             <div class="col-md-8 col-md-pull-4">
                                 <p>Aquarium. Photo by Francisco.Sorbet is the template number 4544 in BTemplates, where a total of 50 003 086 templates has been downloaded since 2008. This template was created by NBThemes and is the...</p><br>
@@ -86,15 +57,65 @@
                     <br>
                     <div class="well" style="background-color: blue">
                         <div class="row" style="margin: auto;">
-                            <img src="img/anuar.png" class="img-responsive" style="margin: auto;">
+                            <h1 class="form-section">Title</h1><hr>
                         </div>
                         <div class="row">
-                            <h1 class="form-section">Title</h1><hr><br>
+                            <img src="img/bg_classmate.jpg" class="img-responsive" style="margin: auto;"><br>
                             <div">
                                 <p>Aquarium. Photo by Francisco.Sorbet is the template number 4544 in BTemplates, where a total of 50 003 086 templates has been downloaded since 2008. This template was created by NBThemes and is the...</p><br>
                                 <a href="#">READ MORE</a><br><br>
                                 <p>Siapa || Tarikh || Masa</p>
                             </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="well" style="height: 60px; background-color: gold; border-style: none;">
+                            <p style="color: red">Masjid Pangsun</p>
+                        </div>
+                        <div class="well" style="margin-top: -22px; font-size: 5em;">
+                            <p style="color: blue;">Borang2 Online</p>
+                        </div>
+                        <div>
+                            <div class="well" style="height: 60px; background-color: gold; border-style: none;">
+                                <p style="color: red">Facebook</p>
+                            </div>
+                            <div style="margin-top: -20px;">
+                                <script>(function(d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id)) {return;}
+                                    js = d.createElement(s); js.id = id;
+                                    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                    }(document, 'script', 'facebook-jssdk'));
+                                </script>
+                                <div class="fb-like-box" data-href="https://www.facebook.com/TahfizPangsun/" data-width="263" data-height="480" data-colorscheme="light" data-show-faces="true" data-stream="true" data-header="true" data-border-color="#ffffff"></div>
+                            </div>
+                        </div>
+                        <br>
+                        <div>
+                            <div class="well" style="height: 60px; background-color: gold; border-style: none;">
+                                <p style="color: red">Video</p>
+                            </div>
+                            <!--Video Masjid-->
+                            <div style="margin-top: -33px;">
+                                <object width="263" height="220"><param name="movie" value="http://pf.kizoa.com/sflite.swf?did=4523837&amp;k=4939993" /><param name="wmode" value="transparent" /><param name="allowFullScreen" value="true" /><embed src="http://pf.kizoa.com/sflite.swf?did=4523837&k=4939993" type="application/x-shockwave-flash" wmode="transparent" width="280" height="260" allowfullscreen="true"></embed></object><br />
+                            </div>
+                        </div>
+                        <br>
+                        <div>
+                            <div class="well" style="height: 60px; background-color: gold; border-style: none;">
+                                <p style="color: red">Hadith & Tazkirah</p>
+                            </div>
+                            <div class="well" style="margin-top: -22px; font-size: 5em;">
+                                <p style="color: blue; text-align: left;">Sabda Rasulullah s.a.w: “Bukanlah (golongan ini) daripada orang beriman yang sempurna imannya : orang yang suka mengecam, suka melaknat yang suka bercakap kufur dan yang suka mencarut”<br>[Riwayat Ahmad]</p>
+                            </div>
+                        </div>
+                        <br>
+                        <div>
+                            <div class="well" style="height: 60px; background-color: gold; border-style: none;">
+                                <p style="color: red">Jumlah Pelawat</p>
+                            </div>
+                                <a href="http://www.reliablecounter.com" target="_blank"><img src="http://www.reliablecounter.com/count.php?page=localhost/masjid/utama&digit=style/plain/24/&reloads=0" alt="www.reliablecounter.com" title="www.reliablecounter.com" border="0" style="height: 70px; width: 263px; margin-top: -20px;"></a>
                         </div>
                     </div>
                 </div>
