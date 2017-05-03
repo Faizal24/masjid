67b2b02e4366b105ea1b4a5e3bb4d236c17381a2
Faizal24 <zalcool24@gmail.com>
2017-05-04 01:41:08 +0800

    Read More

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -8,18 +8,18 @@
     <!-- Stylesheets
     ============================================= -->
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-    <link rel="stylesheet" href="css/style.css" type="text/css" />
-    <link rel="stylesheet" href="css/swiper.css" type="text/css" />
-    <link rel="stylesheet" href="css/dark.css" type="text/css" />
-    <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
-    <link rel="stylesheet" href="css/animate.css" type="text/css" />
-    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/swiper.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/dark.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/magnific-popup.css" type="text/css" />
 
     <!-- Bootstrap Data Table Plugin -->
-    <link rel="stylesheet" href="css/components/bs-datatable.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/components/bs-datatable.css" type="text/css" />
 
-    <link rel="stylesheet" href="css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo base_url();?>css/responsive.css" type="text/css" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Document Title
--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -31,6 +31,51 @@
 	============================================= -->
 	<div id="wrapper" class="clearfix">
 
+	        <!-- Header
+        ============================================= -->
+        <header id="header" class="transparent-header full-header" data-sticky-class="not-dark">
+
+            <div id="header-wrap">
+
+                <div class="container clearfix">
+
+                    <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+                    <!-- Logo
+                    ============================================= -->
+                    <div id="logo">
+                        <a href="<?php echo base_url();?>utama" class="standard-logo" data-dark-logo="images/masjid/masjid_pangsun_small.png"><img src="<?php echo base_url();?>images/masjid/masjid_pangsun_small_brown.png" alt="Masjid Logo"></a>
+                        <a href="<?php echo base_url();?>utama" class="retina-logo" data-dark-logo="images/masjid/full/masjid_pangsun.png"><img src="<?php echo base_url();?>images/masjid/full/masjid_pangsun_brown.png" alt="Masjid Logo"></a>
+                    </div><!-- #logo end -->
+
+                    <!-- Primary Navigation
+                    ============================================= -->
+                    <nav id="primary-menu" class="dark">
+
+                        <ul>
+                            <li><a href="<?php echo base_url();?>utama"><div>Utama</div></a></li>
+                            <li><a href="<?php echo base_url();?>profil"><div>Profil</div></a></li>
+                            <li><a href="<?php echo base_url();?>organisasi"><div>Organisasi</div></a></li>
+                            <li><a href="<?php echo base_url();?>kemudahan"><div>Kemudahan</div></a></li>
+                            <li><a href="#"><div>Perkhidmatan</div></a>
+                                <ul>
+                                    <li><a href="<?php echo base_url();?>jenazah"><div>Pengurusan<br>Jenazah</div></a></li>
+                                    <li><a href="<?php echo base_url();?>jurunikah"><div>Perkhidmatan<br>Jurunikah</div></a></li>
+                                </ul>
+                            </li>
+                            <li><a href="<?php echo base_url();?>jadual"><div>Takwim</div></a></li>
+                            <li><a href="<?php echo base_url();?>maahadtahfiz"><div>Maahad Tahfiz</div></a></li>
+                            <li><a href="<?php echo base_url();?>hubungikami"><div>Hubungi Kami</div></a></li>
+                        </ul>
+
+                    </nav><!-- #primary-menu end -->
+
+                </div>
+
+            </div>
+
+        </header><!-- #header end -->  
+
 		<!-- Content
 		============================================= -->
 		<section id="content">
@@ -68,12 +113,14 @@
 								<!-- Entry Image
 								============================================= -->
 								<div class="entry-image">
-									<a href="#"><img class="thumbnail" src="<?php echo base_url(); ?>admin/uploads/images/<?php echo $post['picture']?>" alt="Blog Single" style="height:300px;width:300px;"></a>
+									<a href="#"><img class="thumbnail" src="<?php echo base_url(); ?>admin/uploads/images/<?php echo $post['picture']?>" alt="Blog Single"></a>
 								</div><!-- .entry-image end -->
 
 								<!-- Entry Content
 								============================================= -->
 								<div class="entry-content notopmargin">
+
+									<p><?php echo $post['content'];?></p>
 
 									<!-- <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
 
@@ -105,7 +152,7 @@
 							<div class="post-navigation clearfix">
 
 								<div class="col_half nobottommargin">
-									<a href="<?php echo base_url() ?>">&lArr; Go Back</a>
+									<a href="<?php echo base_url();?>utama">&lArr; Go Back</a>
 								</div>
 
 							</div><!-- .post-navigation end -->
@@ -116,7 +163,7 @@
 
 					<!-- Sidebar
 					============================================= -->
-<!-- 					<div class="sidebar nobottommargin col_last clearfix">
+					<div class="sidebar nobottommargin col_last clearfix">
 						<div class="sidebar-widgets-wrap">
 							<div class="widget clearfix">
 
@@ -127,7 +174,7 @@
 
 						</div>
 
-					</div> --><!-- .sidebar end -->
+					</div> <!-- .sidebar end-->
 
 				</div>
 
